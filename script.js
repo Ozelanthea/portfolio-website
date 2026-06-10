4df7e75d51c9c768b348a7172e98543c2fffac14
Ozelanthea <ozelanthea@gmail.com>
2026-06-10 14:44:40 +0100

    Mobile responsiveness and hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 const projectCards = document.querySelectorAll(".project-card");
 const nextButton = document.querySelector(".next");
 const previousButton = document.querySelector(".prev");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+})
+
+navLinks.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("open");
+    })
+})
 
 let currentIndex = 0;
 function getCardsToShow() {
